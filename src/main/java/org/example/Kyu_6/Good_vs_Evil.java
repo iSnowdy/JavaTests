9e4222a3a3480e4fb1eea3ec26e2b0a2f7ec4939
Snow <andy0509rey@gmail.com>
2024-02-07 10:59:09 +0100

    Goodies

--- src/main/java/org/example/Kyu_6/Good_vs_Evil.java
+++ src/main/java/org/example/Kyu_6/Good_vs_Evil.java
@@ -1,55 +1,29 @@
-/*
-
 package org.example.Kyu_6;
 
 public class Good_vs_Evil {
     public static String battle(String goodAmounts, String evilAmounts) {
 
-        int[] goodForces = {1, 2, 3, 3, 4, 10};
-        int[] evilForces = {1, 2, 2, 2, 3, 5 ,10};
-        String result = "";
+        int[] goodCount = {1, 2, 3, 3, 4, 10};
+        int[] evilCount = {1, 2, 2, 2, 3, 5, 10};
 
-        System.out.println(goodAmounts.getClass());
-        System.out.println(goodAmounts.charAt(3));
+        int goodTotal = Frodo(goodAmounts.split(" "), goodCount);
+        int evilTotal = Frodo(evilAmounts.split(" "), evilCount);
 
-        String[] goodSplit = goodAmounts.split(" ");
-        String[] evilSplit = evilAmounts.split(" ");
-
-        System.out.println(goodSplit);
-
-        int goodCount = Frodo(goodAmounts.split(" "), goodForces);
-        int evilCount = Frodo(evilAmounts.split(" "), evilForces);
-
-        if (goodCount > evilCount) {
+        if (goodTotal > evilTotal) {
             return "Battle Result: Good triumphs over Evil";
-        }
-
-        else if (evilCount > goodCount) {
+        } else if (goodTotal < evilTotal) {
             return "Battle Result: Evil eradicates all trace of Good";
+        } else {
+            return "Battle Result: No victor on this battle field";
         }
+    }
 
-        else {
-            return "Battle Result: No victor on this battle field"
+    public static int Frodo(String[] units, int[] worth) {
+
+        int counter = 0;
+        for (int i = 0; i < units.length; i ++) {
+            counter = counter + (Integer.parseInt(units[i]) * worth[i]);
         }
-
-
-
-        private static int Frodo(String[] count, int[] worth) {
-            int value = 0;
-            for (int i = 0; i < count.length; i ++) {
-                value = value + (Integer.parseInt(count[i]) * worth);
-            }
-
-            return value;
-
-
-
-
-
-
-
+        return counter;
     }
 }
-
-*/
-
